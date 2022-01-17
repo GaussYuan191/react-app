ce9367a1662bb1441dd6ad38fe2ef2d46e8d7ab4
GaussYuan191 <1915059302@qq.com>
2022-01-17 09:48:35 +0800

    新增

--- src/views/Manage/ManageLicense/index.jsx
+++ src/views/Manage/ManageLicense/index.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-import { Form, Button, Select, Row, Col, Space, Divider, Table, Modal} from 'antd';
+import {
+	Form,
+	Button,
+	Select,
+	Row,
+	Col,
+	Space,
+	Divider,
+	Table,
+	Modal,
+	Tag,
+} from 'antd';
 import './index.less';
-import {getLicenseList} from '@/api/getLicenseList.js'
+import { getLicenseList } from '@/api/getLicenseList.js';
+import { listAllSub } from '@/api/listAllSub.js';
+import { queryFunctionCode } from '@/api/queryFunctionCode.js';
+import { exportLicense } from '@/api/exportLicense.js';
 const { Option } = Select;
 const { Column } = Table;
 let data = [
@@ -26,15 +40,78 @@ let data = [
 ];
 export default class ManageLicense extends Component {
 	formRef = React.createRef();
-    state = {licenseList: [], visible: false, licenseNode: {} }
+	state = {
+		licenseList: [],
+		visible: false,
+		licenseNode: {},
+		licenseDefault: data,
+		licenseData: [],
+		selectNode: [],
+	};
 	onFinish = (values) => {
-		console.log(values);
+		let { licenseData } = this.state;
+		let { permission } = values;
+		let isPermission = permission == 'informal' ? '1' : '2';
+		let newLicenseList = licenseData.filter((item) => {
+			return item.permission == isPermission;
+		});
+		console.log(newLicenseList);
+		this.setState({ licenseList: newLicenseList });
 	};
 	onReset = () => {
+		let { licenseData } = this.state;
+		this.setState({ licenseList: licenseData });
 		this.formRef.current.resetFields();
 	};
 	downloadLicense = () => {
-		console.log('开始下载');
+		const { selectNode } = this.state;
+		let ids = selectNode.join();
+		exportLicense(ids).then(
+			(res) => {
+				console.log('222');
+
+				const base64ToBlob=(code)=>{
+					const raw=window.atob(code)
+					const rawLength=raw.length
+					const uInt8Array = new Int8Array(rawLength)
+					for(let i=0;i<rawLength;i++){
+						uInt8Array[i]=raw.charCodeAt(i)
+					}
+					return new Blob([uInt8Array])
+				}
+
+				let reader = new FileReader();
+				const blob = new Blob([res]);
+				reader.readAsText(blob);
+				console.log('222');
+				reader.onload = (e) => {
+					console.log(e);
+					if (JSON.parse(e.target.result).status === 200) {
+						const fileName = '附件名'; //附件名包含附件类型
+						console.log('222');
+						if ('download' in document.createElement('a')) {
+							console.log('下载成功');
+							console.log('aaa', e.target.result)
+							let data = base64ToBlob(JSON.parse(e.target.result));
+							const link = document.createElement('a');
+							link.download = fileName;
+							link.style.display = 'none';
+							link.href = URL.createObjectURL(data);
+							document.body.appendChild(link);
+							link.click();
+							URL.revokeObjectURL(link.href);
+							document.body.removeChild(link);
+						} else {
+							navigator.msSaveBlob(data, fileName);
+						}
+					}
+				};
+			},
+			(err) => {
+				console.log(err);
+			}
+		);
+		console.log('开始下载', ids);
 	};
 	updateLicense = () => {
 		console.log('开始更新');
@@ -42,69 +119,140 @@ export default class ManageLicense extends Component {
 	deleteLicense = () => {
 		console.log('开始删除');
 	};
-    // 显示数据条数
+	// 显示数据条数
 	showTotal = (total) => {
 		return `共 ${total} 条`;
 	};
-    // 显示详细的权限树 
-    showPermission = (res) => {
-        let {licenseNode} = this.state;
-        return () => {
-            console.log(res);
-            data[0].children = res.customerLicenseFunctionList;
-            data[1].children = res.customerLicenseDataList;
-            data[2].children = res.customerLicenseInterfaceList;
-            // licenseNode = data
-            console.log(data);
-            this.showModal();
-        }
-        
-        
-    }
-    showModal = () => {
-        // setVisible(true);
-        this.setState({visible: true})
-    };
-    handleCancel = () => {
+	// 显示详细的权限树
+	showPermission = (res) => {
+		return () => {
+			console.log('选择的节点', res);
+			this.setState({ licenseNode: res });
+			this.showModal();
+		};
+	};
 
-        console.log('Clicked cancel button');
-        this.setState({visible: false, licenseNode: {}})
-    };
-    
-    componentDidMount = () => {
-        this.dataInit();
-    }
-    dataInit = () => {
-        getLicenseList(1, 1000).then(res => {
-            console.log(res); 
-            res.list.map(item=> {
-                item.key = item.id;
-            })
-            this.setState({licenseList: res.list})
-        })
-    }
+	// 处理树形数据
+	dealTree = (nums) => {
+		if (nums == null) return;
+		if (nums.children != null) {
+			nums.children.forEach((item) => {
+				this.dealTree(item);
+			});
+		}
+		nums.name = nums.menuName || nums.dicExplain;
+		nums.key = nums.menuCode || nums.dicCode;
+		nums.right = 1; // 初始化权限
+	};
+	showModal = () => {
+		this.setState({ visible: true });
+	};
+	handleCancel = () => {
+		console.log('Clicked cancel button');
+		this.setState({ visible: false, licenseNode: {} });
+	};
+	// 记录选中的值的ID
+	onChange = (value) => {
+		this.setState({ selectNode: value });
+	};
+	// 深拷贝
+	deepCopy = (obj) => {
+		if (!obj || typeof obj !== 'object') return;
+		let newObj = Array.isArray(obj) ? [] : {};
+		for (let key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				newObj[key] =
+					typeof obj[key] === 'object' ? this.deepCopy(obj[key]) : obj[key];
+			}
+		}
+
+		return newObj;
+	};
+
+	componentDidMount = () => {
+		this.dataInit();
+	};
+	dataInit = () => {
+		let { licenseDefault } = this.state;
+		let resultList = [];
+		resultList.push(queryFunctionCode());
+		resultList.push(listAllSub(1050006));
+		resultList.push(listAllSub(1030308));
+		Promise.all(resultList).then(
+			(res) => {
+				res.forEach((arr, index) => {
+					arr.forEach((item) => {
+						this.dealTree(item);
+					});
+					licenseDefault[index].children = arr;
+				});
+				this.setState({ licenseDefault: licenseDefault });
+			},
+			(err) => {
+				console.log(err);
+			}
+		);
+
+		getLicenseList(1, 1000).then((res) => {
+			console.log(res);
+			res.list.map((item) => {
+				item.key = item.id;
+			});
+			this.setState({ licenseList: res.list, licenseData: res.list });
+		});
+	};
 	render() {
-        const {licenseList, visible, licenseNode, } = this.state;
-        console.log(visible)
+		const { licenseList, visible, licenseNode, licenseDefault } = this.state;
+		console.log(visible);
 		return (
 			<div>
-                <Modal
-                    title="许可明细"
-                    visible={visible}
-
-                    onCancel={this.handleCancel}
-                >
-                    <Table
-						
+				<Modal
+					title="许可明细"
+					visible={visible}
+					onCancel={this.handleCancel}
+					width={800}
+					footer={null}
+					destroyOnClose="true"
+				>
+					<Table
 						rowSelection={undefined}
 						pagination={{ position: ['none', 'none'] }}
 						scroll={{ y: 400 }}
-						dataSource={licenseNode}
+						dataSource={licenseDefault}
+						defaultExpandAllRows="true"
+						bordered="true"
 					>
-<Column title="许可名称" dataIndex="name" key="name" />
-<Column title="许可授权" dataIndex="right" key="right" />
-                    </Table>
-                </Modal>
+						<Column title="许可名称" dataIndex="name" key="name" />
+						<Column
+							title="许可授权"
+							dataIndex="right"
+							key="right"
+							render={(text, record) => {
+								if (record.children == null) {
+									let hasPermission =
+										licenseNode.customerLicenseDataList.some((item) => {
+											return item.code == record.key && item.right == 1;
+										}) ||
+										licenseNode.customerLicenseInterfaceList.some((item) => {
+											return item.code == record.key && item.right == 1;
+										}) ||
+										licenseNode.customerLicenseFunctionList.some((item) => {
+											return item.code == record.key && item.right == 1;
+										});
+									record.right = hasPermission ? 1 : 0;
+									return (
+										<Tag
+											color={record.right == 1 ? 'success' : 'default'}
+											key={record.key}
+										>
+											{record.right == 1 ? '有权限' : '无权限'}
+										</Tag>
+									);
+								}
+							}}
+						/>
+					</Table>
+				</Modal>
 				<Form ref={this.formRef} name="selectLicense" onFinish={this.onFinish}>
 					<Row>
 						<Col span={8}>
@@ -124,7 +272,7 @@ export default class ManageLicense extends Component {
 							<Form.Item>
 								<Space size={20}>
 									<Button type="primary" htmlType="submit">
-										提交
+										查询
 									</Button>
 									<Button htmlType="button" onClick={this.onReset}>
 										重置
@@ -135,7 +283,7 @@ export default class ManageLicense extends Component {
 					</Row>
 				</Form>
 				<Divider />
-				<Space size={20} style={{marginBottom: 20}}>
+				<Space size={20} style={{ marginBottom: 20 }}>
 					<Button
 						type="primary"
 						htmlType="button"
@@ -150,25 +298,37 @@ export default class ManageLicense extends Component {
 						删除
 					</Button>
 				</Space>
-                {
-                    console.log(licenseList)
-                }
-				<Table dataSource={licenseList} bordered="true" size="middle" pagination={{
-								total: licenseList.length,
-								pageSize: 10,
-								showTotal: this.showTotal,
-							}} >
-					<Column title="客户名" dataIndex="customerName" key="customerName" align="center" />
-					<Column title="许可类型" dataIndex="permission" key="permission"  align="center"
-                    render={(text, record) => {
-                        const {permission} = record;
-                        if (permission == 1) {
-                            return <span>试用</span>
-                        } else {
-                            return <span>正式</span>
-                        }
-                    }}
-                    />
+				<Table
+					dataSource={licenseList}
+					bordered="true"
+					size="middle"
+					rowSelection={({ type: 'checkbox' }, { onChange: this.onChange })}
+					pagination={{
+						total: licenseList.length,
+						pageSize: 10,
+						showTotal: this.showTotal,
+					}}
+				>
+					<Column
+						title="客户名"
+						dataIndex="customerName"
+						key="customerName"
+						align="center"
+					/>
+					<Column
+						title="许可类型"
+						dataIndex="permission"
+						key="permission"
+						align="center"
+						render={(text, record) => {
+							const { permission } = record;
+							if (permission == 1) {
+								return <span>试用</span>;
+							} else {
+								return <span>正式</span>;
+							}
+						}}
+					/>
 					<Column
 						title="试用开始时间"
 						dataIndex="beginDate"
@@ -177,35 +337,36 @@ export default class ManageLicense extends Component {
 					/>
 					<Column
 						title="试用结束日期"
-                        dataIndex="endDate"
+						dataIndex="endDate"
 						key="endDate"
 						align="center"
 					/>
-                    <Column
+					<Column
 						title="创建人"
-                        dataIndex="createUserId"
+						dataIndex="createUserId"
 						key="createUserId"
 						align="center"
 					/>
-                    <Column
+					<Column
 						title="	创建时间"
-                        dataIndex="createTime"
+						dataIndex="createTime"
 						key="createTime"
 						align="center"
 					/>
-                    <Column
+					<Column
 						title="操作"
-                        dataIndex="demo"
+						dataIndex="demo"
 						key="demo"
 						align="center"
-                        render = {(text, record) => {
-                          
-                            return <Button type="primary" onClick={this.showPermission(record)}>查看详细</Button>
-                        }}
+						render={(text, record) => {
+							return (
+								<Button type="primary" onClick={this.showPermission(record)}>
+									查看详细
+								</Button>
+							);
+						}}
 					/>
 				</Table>
-                
-				
 			</div>
 		);
 	}
