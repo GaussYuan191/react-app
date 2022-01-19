eb0e4bc059117caae75c78b1d148e40bc08bade9
GaussYuan191 <1915059302@qq.com>
2022-01-19 17:30:34 +0800

    修改 数据导出接口

--- src/api/exportLicense.js
+++ src/api/exportLicense.js
@@ -1,13 +1,14 @@
-import request from '@/utils/request';
+import download from '@/utils/download';
 // 数据字典子项查询接口
 export function exportLicense(ids) {
-	return request({
+	return download({
 		url: '/license/generator/export',
 		method: 'post',
 		data: {  
         },
 		params: {
 			ids:ids
-		}
+		},
+		responseType: 'blob'
 	});
 }
