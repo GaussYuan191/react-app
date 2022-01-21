f0154a885e64dbffc2d19dd83fc68d43597fdc5d
GaussYuan191 <1915059302@qq.com>
2022-01-21 18:12:34 +0800

    完善

--- src/componments/FromManage/index.jsx
+++ src/componments/FromManage/index.jsx
@@ -2,18 +2,14 @@ import React, { Component } from 'react';
 import { Form, Input } from 'antd';
 
 export default class FromManage extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.formDataRef = React.createRef();
 		this.props.wrappedComponentRef(this.formDataRef);
 	}
 	render() {
 		return (
-			<Form
-				name="basic"
-				autoComplete="off"
-				ref={this.formDataRef}
-			>
+			<Form name="basic" autoComplete="off" ref={this.formDataRef}>
 				<Form.Item
 					label="客户名称"
 					name="name"
@@ -24,7 +20,7 @@ export default class FromManage extends Component {
 						},
 					]}
 				>
-				<Input />
+					<Input placeholder="客户名全称(限20位)" />
 				</Form.Item>
 
 				<Form.Item
@@ -35,8 +31,9 @@ export default class FromManage extends Component {
 							required: true,
 							message: '请输入客户编码',
 						},
-					]}>
-					<Input />
+					]}
+				>
+					<Input placeholder="简称/编码(限18位)" />
 				</Form.Item>
 			</Form>
 		);
