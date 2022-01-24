8070ffbc1343678ae23972cd99bab50f486d2f23
GaussYuan191 <1915059302@qq.com>
2022-01-24 16:51:12 +0800

    完善

--- src/api/getPageList.js
+++ src/api/getPageList.js
@@ -1,12 +1,13 @@
 import request from '@/utils/request';
 
-export function getPageList(reqPageNum, reqPageSize) {
+export function getPageList(reqPageNum, reqPageSize, name) {
 	return request({
 		url: '/customer/pageList',
 		method: 'post',
 		data: {
 			reqPageNum: reqPageNum,
 			reqPageSize: reqPageSize,
+			name: name
 		},
 	});
 }
